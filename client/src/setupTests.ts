7440d7f59eecab36c2c255e0e68231fc951dede8
Kekman <baxodirovich777@gmail.com>
2022-01-20 15:43:31 +0300

    fix enzyme dependence

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -1,3 +1,3 @@
-import * as enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import * as enzyme from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 enzyme.configure({ adapter: new Adapter() });
