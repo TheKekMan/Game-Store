e2cce4cf4377cedbfa62698b4da10c31d6c3b39b
Kekman <baxodirovich777@gmail.com>
2022-01-20 09:42:53 +0300

    refactor routes for backend

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,12 @@
-const Router = require('express').Router()
-const GamesRouter = require('./games')
-const UserRouter = require('./user')
-const AuthRouter = require('./auth')
-const authUser = require('../Services/Auth')
-const auth = require('../middleware/auth');
+const Router = require("express").Router();
+const GamesRouter = require("./games");
+const UserRouter = require("./user");
+const AuthRouter = require("./auth");
 
-Router.use('/games', GamesRouter)
+Router.use("/games", GamesRouter);
 
-Router.use('/user', UserRouter)
+Router.use("/user", UserRouter);
 
-Router.use('/auth', AuthRouter)
+Router.use("/auth", AuthRouter);
 
-module.exports = Router
+module.exports = Router;
