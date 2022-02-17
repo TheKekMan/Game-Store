eaa6bae0a502951476932f9b620578d8a98b651a
Kekman <baxodirovich777@gmail.com>
2022-02-17 10:24:14 +0300

    deploy init

---
+++ client/webpack.i18n.js
@@ -0,0 +1,68 @@
+const Path = require("path");
+const HtmlWebpack = require("html-webpack-plugin");
+require("i18next");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".tsx", ".js"],
+  },
+  output: {
+    path: Path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.(css)$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(png|jpg)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ["babel-loader", "react-svg-loader"],
+      },
+      {
+        test: /\.(tsx)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              [
+                "@babel/preset-typescript",
+                {
+                  runtime: "automatic",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  mode: "production",
+  plugins: [
+    new HtmlWebpack({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    proxy: {
+      "/gamestore/api": {
+        target: "http://localhost:5500",
+        changeOrigin: true,
+      },
+    },
+    historyApiFallback: true,
+    allowedHosts: "all",
+  },
+};
